c64039488bafafbe12ba3409fb151973cd9e07ca
Astro <astro@spaceboyz.net>
2017-08-24 23:51:14 +0200

    Shared

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate opencv;
 extern crate midir;
 
 use std::thread;
+use std::sync::{Arc, RwLock};
 
 use opencv::core as cv;
 use opencv::sys::types as cvtypes;
@@ -10,6 +11,22 @@ use opencv::{imgproc, objdetect, highgui};
 use midir::{MidiInput, MidiOutput, Ignore};
 use midir::os::unix::{VirtualInput, VirtualOutput};
 
+struct Shared<T>(Arc<RwLock<T>>);
+
+impl<T> Clone for Shared<T> {
+    fn clone(&self) -> Self {
+        Shared(self.0.clone())
+    }
+}
+
+unsafe impl<T> Send for Shared<T> {}
+
+impl<T> Shared<T> {
+    pub fn new (t: T) -> Self {
+        Shared(Arc::new(RwLock::new(t)))
+    }
+}
+
 //static mut the_matrix : Option<cv::Mat> = None; // : opencv::core::Mat<opencv::core::Mat<Vec3b>;
 
 struct Cursor {
@@ -17,7 +34,7 @@ struct Cursor {
 	y : i32
 }
 
-fn run_webcam(matrix : &cv::Mat, cursor : &Cursor) -> Result<(), String> {
+fn run_webcam(shared_matrix : Shared<cv::Mat>, shared_cursor : Shared<Cursor>) -> Result<(), String> {
 	let mut classifier =
 		try!(objdetect::CascadeClassifier::for_file("haarcascade_frontalface_alt.xml"));
 
@@ -32,9 +49,11 @@ fn run_webcam(matrix : &cv::Mat, cursor : &Cursor) -> Result<(), String> {
 			let mut image0 = cv::mat();
 			try!(capture.retrieve(&mut image0, 0));
 			//let mut image1 = cv::mat();
-			let dest = opencv::core::Size {width: 16, height: 16};
+		        let dest = opencv::core::Size {width: 16, height: 16};
+                        let mut matrix = shared_matrix.0.write().unwrap();
 			try!(imgproc::resize(&image0, &mut matrix, dest, 0.0, 0.0, 0));
 
+                        let cursor = shared_cursor.0.read().unwrap();
 			unsafe {
 				let row = matrix.ptr(y).unwrap();
 				let mut pix = row.offset(3 * cursor.x as isize);
@@ -54,7 +73,7 @@ fn run_webcam(matrix : &cv::Mat, cursor : &Cursor) -> Result<(), String> {
 	Ok(())
 }
 
-fn midi_worker(matrix : &cv::Mat, cursor : &Cursor) {
+fn midi_worker(shared_matrix : Shared<cv::Mat>, shared_cursor : Shared<Cursor>) {
 	let midi_out = MidiOutput::new("MidiCam").unwrap();
 	let mut conn_out = (midi_out.create_virtual("midicam").map_err(|e| e.kind())).unwrap();
 	
@@ -62,10 +81,13 @@ fn midi_worker(matrix : &cv::Mat, cursor : &Cursor) {
 	conn_out.send(&[192, 0]);
 
 	loop {
-		//let note = *pix / 2;
+                let matrix = shared_matrix.0.read().unwrap();
+                let mut cursor = shared_cursor.0.write().unwrap();
+	        //let note = *pix / 2;
 		let mut row = matrix.ptr(cursor.y).unwrap();
 		unsafe {
-			let mut pix = row.offset(3 * cursor.x as isize);
+		        let mut pix = row.offset(3 * cursor.x as isize);
+                        println!("pix at {}", 3 * cursor.x);
 			let note = *pix / 2;
 			
 			println!("Sending Midi note {}", note);
@@ -82,14 +104,14 @@ fn midi_worker(matrix : &cv::Mat, cursor : &Cursor) {
 }
 
 fn main() {
-	let mut cursor = Cursor {x: 0, y: 0};
-
-	let the_matrix = cv::mat();
-
+	let cursor = Shared::new(Cursor {x: 0, y: 0});
+        let cursor_ = cursor.clone();
+	let the_matrix = Shared::new(cv::mat());
+        let the_matrix_ = the_matrix.clone();
 	thread::spawn(move || {
 		println!("I'm in a thread, doing stuff");
-		midi_worker(&the_matrix, &cursor);
+		midi_worker(the_matrix_, cursor_);
 	});
 
-    run_webcam(&the_matrix, &cursor).unwrap()
+    run_webcam(the_matrix, cursor).unwrap()
 }
